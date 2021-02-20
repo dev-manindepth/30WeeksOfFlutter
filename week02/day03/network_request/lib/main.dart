774b9ccc19acfef3dd34da9dc1dae54db056c4b3
dev-manindepth <manindepth07@gmail.com>
2021-02-20 12:16:01 +0530

    http get request

---
+++ week02/day03/network_request/lib/main.dart
@@ -0,0 +1,41 @@
+
+import 'package:flutter/material.dart';
+
+import 'data_service.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final _dataService = DataService();
+  String _response;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(body: Center(
+        child: Builder(builder: (_) {
+          if (_response != null) {
+            return Text(_response);
+          } else {
+            return ElevatedButton(
+              child: Text('Make Request'),
+              onPressed: _makeRequest,
+            );
+          }
+        }),
+      )),
+    );
+  }
+
+  void _makeRequest() async {
+    final response = await _dataService.makeRequestToApi();
+    setState(() => _response = response);
+  }
+}
