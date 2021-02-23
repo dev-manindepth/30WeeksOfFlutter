2c15a52f168e3a0ebb13726948f7604caa7841bf
dev-manindepth <manindepth07@gmail.com>
2021-02-23 19:24:38 +0530

    ListView Builder,List Generate,Card & Assert

---
+++ week02/day05/catalog_app/lib/pages/home_page.dart
@@ -0,0 +1,30 @@
+import 'package:catalog_app/models/catalog.dart';
+import 'package:catalog_app/widgets/drawer.dart';
+import 'package:catalog_app/widgets/item_widget.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  final int days = 30;
+  final String name = "Manish";
+  @override
+  Widget build(BuildContext context) {
+    final dummyList = List.generate(20, (index) => CatalogModel.items[0]);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Catalog App"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView.builder(
+          itemCount: dummyList.length,
+          itemBuilder: (context, index) {
+            return ItemWidget(
+              item: dummyList[index],
+            );
+          },
+        ),
+      ),
+      drawer: MyDrawer(),
+    );
+  }
+}
