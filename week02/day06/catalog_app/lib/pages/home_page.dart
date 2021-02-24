ec683328cee9c802808a7be04f2faa90a6da2853
dev-manindepth <manindepth07@gmail.com>
2021-02-24 21:32:24 +0530

    local files,decode json

---
+++ week02/day06/catalog_app/lib/pages/home_page.dart
@@ -0,0 +1,53 @@
+import 'package:catalog_app/models/catalog.dart';
+import 'package:catalog_app/widgets/drawer.dart';
+import 'package:catalog_app/widgets/item_widget.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'dart:convert';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final int days = 30;
+
+  final String name = "Manish";
+
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    final catalogJson =
+        await rootBundle.loadString("assets/files/catalog.json");
+    final decodeData = jsonDecode(catalogJson);
+    var productsData = decodeData["products"];
+    print(productsData);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final dummyList = List.generate(20, (index) => CatalogModel.items[0]);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Catalog App"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView.builder(
+          itemCount: dummyList.length,
+          itemBuilder: (context, index) {
+            return ItemWidget(
+              item: dummyList[index],
+            );
+          },
+        ),
+      ),
+      drawer: MyDrawer(),
+    );
+  }
+}
